af9b08f74dcd890efd2eacecbc2fda7ff281c202
HJtheKing <09hyen28@gmail.com>
2023-01-31 02:44:51 +0900

    4th

--- src/main/java/prac/prac01/controller/HelloController.java
+++ src/main/java/prac/prac01/controller/HelloController.java
@@ -1,4 +1,4 @@
-package prac.prac01.Controller;
+package prac.prac01.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
---
+++ src/main/java/prac/prac01/repository/MemberRepository.java
@@ -0,0 +1,17 @@
+package prac.prac01.repository;
+
+
+import prac.prac01.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+
+    Optional<Member> findById(Long id);
+
+    Optional<Member> findByName(String name);
+
+    List<Member> findAll();
+}
